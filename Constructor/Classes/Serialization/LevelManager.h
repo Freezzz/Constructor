86bdccb38aeb216f6c0132acd04544359c7f87ef
freezzz <freezzz.nospam@gmail.com>
2012-04-24 04:20:14 +0200

    Level Manager sketch

---
+++ Constructor/Classes/Serialization/LevelManager.h
@@ -0,0 +1,50 @@
+//
+//  LevelManager.h
+//  Constructor
+//
+//  Created by Ivan Litsvinenka on 4/24/12.
+//  Copyright (c) 2012 The Epic Bean. All rights reserved.
+//
+
+#ifndef Constructor_LevelManager_h
+#define Constructor_LevelManager_h
+#include "cocos2d.h"
+#include "LevelDef.h"
+
+using namespace cocos2d;
+
+typedef struct StoryLevelDescribtion{
+	bool isConmplete;
+	string name;
+}StoryLevelDescribtion;
+
+class LevelManager {
+private:
+	vector<string*> m_userLevels;
+	vector<StoryLevelDescribtion*> m_storyLevels;
+	
+	unsigned int m_userLevelCount;
+	unsigned int m_storyLevelCount;
+	
+	void loadUserLevelList();
+	void loadStoryLevelList();
+	
+public:
+	LevelManager( );
+	~LevelManager( );
+
+	LevelDef *loadUserLevel( const char *fileName );
+	bool saveUserLevel( LevelDef * level, const char *fileName );
+	
+	vector<string*> * getUserLevelList(){return &m_userLevels;}
+	vector<StoryLevelDescribtion*> * getStoryLevelList(){return &m_storyLevels;}
+	
+	//////////////////////////////////////////////////// 
+    // Singleton pattern
+    //////////////////////////////////////////////////// 
+	static LevelManager* levelManagerInstance;
+	static LevelManager* sharedManager();    
+
+};
+
+#endif
