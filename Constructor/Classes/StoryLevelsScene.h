72d6e72a7b15edb541bf6857c894fd4b64eb33fb
freezzz <freezzz.nospam@gmail.com>
2012-04-26 03:01:52 +0200

    Level selection scene

---
+++ Constructor/Classes/StoryLevelsScene.h
@@ -0,0 +1,34 @@
+//
+//  StoryLevelsScene.h
+//  Constructor
+//
+//  Created by Ivan Litsvinenka on 4/23/12.
+//  Copyright 2012 The Epic Bean. All rights reserved.
+//
+
+#ifndef __StoryLevelsScene_H__
+#define __StoryLevelsScene_H__
+
+#include "cocos2d.h"
+using namespace cocos2d;
+
+
+
+////////////////////////////////////////////////////
+// StoryLevelsScene
+///////////////////////////////////////////////////
+class StoryLevelsScene : public CCLayer {
+protected:
+	virtual bool init();
+	
+	void levelSelectedTap( CCObject * sender );
+	void mainMenuTap( CCObject * sender );
+public:
+	////////////////////////////////////////////////////
+	// Static factory creation methods
+	////////////////////////////////////////////////////
+	static CCScene* scene();
+	LAYER_NODE_FUNC(StoryLevelsScene);
+};
+
+#endif
